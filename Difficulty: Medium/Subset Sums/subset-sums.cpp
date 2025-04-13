ae0b28b82db0735593fc7d84a76eaebb714c983c
LakhanSinghRana <165405938+LakhanSinghRana@users.noreply.github.com>
2025-04-13 21:22:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void subsetSum(int i, int sum, vector<int>& arr, vector<int>& result) {
+        if (i == arr.size()) {
+            result.push_back(sum);
+            return;
+        }
+
+    // Include current element
+        subsetSum(i + 1, sum + arr[i], arr, result);
+
+    // Exclude current element
+        subsetSum(i + 1, sum, arr, result);
+    }
+    vector<int> subsetSums(vector<int>& arr) {
+        // code here
+        vector<int> v;
+        subsetSum(0,0,arr,v);
+        return v;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+
+    while (t--) {
+        vector<int> inputArray;
+        string inputLine;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, inputLine);
+        stringstream inputStream(inputLine);
+        int num;
+        while (inputStream >> num) {
+            inputArray.push_back(num); // Read the array elements from input string
+        }
+
+        Solution solutionObject;
+        vector<int> result = solutionObject.subsetSums(inputArray);
+        sort(result.begin(), result.end());
+
+        for (int i = 0; i < result.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << result[i];
+        }
+        cout
+            << endl
+            << "~\n"; // Print results in list format with new line after each test case
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
