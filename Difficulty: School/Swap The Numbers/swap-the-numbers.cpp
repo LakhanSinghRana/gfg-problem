46dfe4a270f9392086d7eaede026edc57cefe714
LakhanSinghRana <165405938+LakhanSinghRana@users.noreply.github.com>
2025-04-16 22:34:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.cpp
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    // Write Code to Swap
+    int temp=a;
+    a=b;
+    b=temp;
+
+    cout << a << " " << b << endl;
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
