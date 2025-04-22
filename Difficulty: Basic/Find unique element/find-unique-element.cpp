544f8ff29c4cdc1041eeaf6160cbed15b328e681
LakhanSinghRana <165405938+LakhanSinghRana@users.noreply.github.com>
2025-04-22 21:15:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find unique element/find-unique-element.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findUnique(int k, vector<int>& arr) {
+        // code here
+        map<int,int> m;
+        for(auto x: arr){
+            m[x]++;
+        }
+        for(auto x: m){
+            if(x.second!=k) return x.first;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after the number of test cases
+    while (t--) {
+        int k;
+        cin >> k;
+        cin.ignore(); // Ignore the newline character after k
+        string inputLine;
+        getline(cin, inputLine);
+        stringstream ss(inputLine);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution obj;
+        cout << obj.findUnique(k, arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
